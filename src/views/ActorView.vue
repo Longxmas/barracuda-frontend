b0ddd4dcc6aec3ae863614877a7e25c3544712ca
Longxmas <20376208@buaa.edu.cn>
2022-10-22 12:02:19 +0800

    first commit

---
+++ src/views/ActorView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card>
+    <v-card-text>
+        !!
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Actor.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
